179c6a693b18a6d5966c734194c959108e0ac56c
Antònia Maria Axartell Carsi <antonia.maria.axartell@estudiantat.upc.edu>
2025-06-25 16:54:01 +0200

    Add files via upload
    Aquest codi genera una nova imatge combinant una màscara binària i una imatge en escala de grisos. Manté els valors originals de la imatge de grisos només on la màscara té valor 255 (zones de  posidònia) i assigna 0 (negre) a la resta. El resultat és una imatge que mostra únicament les àrees d’interès, ressaltant-les sobre un fons negre. Finalment, es desa la imatge processada. (3 juliol de 2023).

---
+++ reultat0mesgran.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 13 17:27:23 2025
+
+@author: Moi
+"""
+
+from PIL import Image
+import numpy as np
+
+# Carregar les imatges
+mask_path = r"C:\Users\Moi\Desktop\tfg\videoimatgesvilanova\posidoniavilanova3juliol20234bandas\codis\outputmasgrande.png"
+grayscale_path = r"C:\Users\Moi\Desktop\tfg\videoimatgesvilanova\posidoniavilanova3juliol20234bandas\codis\Blue-Nir_resta2023\Blue_Corrected2_cropped.png"
+output_path = r"C:\Users\Moi\Desktop\tfg\videoimatgesvilanova\posidoniavilanova3juliol20234bandas\codis\resultat0_2023_mesgran.png"
+
+mask = Image.open(mask_path).convert("L")  
+grayscale = Image.open(grayscale_path).convert("L")
+
+# Convertir a arrays numpy
+mask_array = np.array(mask)
+grayscale_array = np.array(grayscale)
+
+# Aplicar la condició: si mask és 0 → 0, si és 255 → valor de la imatge en escala de grisos
+output_array = np.where(mask_array == 0, 0, grayscale_array)
+
+# Convertir a imatge
+output_image = Image.fromarray(output_array.astype(np.uint8))
+
+# Guardar la imatge
+output_image.save(output_path)
+
+print(f"Imatge generada i guardada a {output_path}")
