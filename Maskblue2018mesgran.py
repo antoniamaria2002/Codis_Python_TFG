ee8129f013f79cebb6aa74ba790e0c145652db66
Antònia Maria Axartell Carsi <antonia.maria.axartell@estudiantat.upc.edu>
2025-06-25 17:09:48 +0200

    Add files via upload
    Aquest codi en Python aplica una segmentació per llindar sobre una imatge en escala de grisos per detectar automàticament zones de Posidònia oceànica. Primer, extreu els píxels diferents de zero dins la màscara i representa un histograma per analitzar les intensitats. A partir d’un llindar definit (en aquest cas 28), genera una màscara binària on els píxels amb intensitat baixa (característics de la Posidònia) es marquen com a blancs (255). Finalment, desa la imatge segmentada i la mostra juntament amb la imatge original millorada. (imatge 10 juliol de 2018).

---
+++ Maskblue2018mesgran.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 13 17:57:27 2025
+
+@author: Moi
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# Ruta de la imatge
+image_path = r"C:\Users\Moi\Desktop\tfg\videoimatgesvilanova\10juriol2018\imatges\resultat0_2018_mesgran.png"
+
+# Carregar la imatge de la banda blava amb la màscara aplicada 
+blue_masked = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Comprovació bàsica per verificar si s'ha carregat la imatge correctament
+if blue_masked is None:
+    raise ValueError(f"No s'ha pogut carregar la imatge. Comprova la ruta: {image_path}")
+
+# Pas 1: Extreure només els píxels diferents de 0 
+blue_values = blue_masked[blue_masked > 0]
+
+# Pas 2: Crear un histograma per observar el contrast entre aigua i Posidònia
+plt.hist(blue_values, bins=40, color='blue', alpha=0.7)
+plt.title("Histograma de la Banda Blava dins la Màscara de Posidònia")
+plt.xlabel("Intensitat del píxel")
+plt.ylabel("Freqüència")
+plt.grid(True)
+plt.show()
+
+# Pas 3: Aplicar un llindar (threshold) per detectar Posidònia
+threshold = 28 
+posidonia_binary = np.zeros_like(blue_masked)
+posidonia_binary[(blue_masked > 0) & (blue_masked < threshold)] = 255  
+
+
+# Pas 4: Guardar i visualitzar el resultat
+output_image_path = r"C:\Users\Moi\Desktop\tfg\videoimatgesvilanova\10juriol2018\imatges\Detected_Posidonia_2018_mesgran.png"
+cv2.imwrite(output_image_path, posidonia_binary)
+
+# Mostrar les imatges: original millorada i resultat de la detecció
+fig, ax = plt.subplots(2, 1, figsize=(24, 12))
+ax[0].imshow(blue_masked, cmap="gray")
+ax[0].set_title("Imatge Millorada")
+ax[0].axis("off")
+
+ax[1].imshow(posidonia_binary, cmap="gray")
+ax[1].set_title("Àrea Detectada de *Posidònia*")
+ax[1].axis("off")
+
+plt.tight_layout()
+plt.show()
